c3dc433de3a3d7129d4c415842b8412645d2c6cf
iuliancarnaru <iulian.carnaru@gmail.com>
2021-05-25 22:29:31 +0100

    project structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,49 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
+import * as esbuild from "esbuild-wasm";
+import { render } from "react-dom";
 
 const App: React.FC = () => {
-  return <h1>HI</h1>
-}
+  const ref = useRef<any>();
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
 
-render(<App />, document.getElementById('root'))
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "./esbuild.wasm",
+    });
+  };
+
+  const onButtonClick = async () => {
+    if (!ref.current) return;
+
+    const result = await ref.current.transform(input, {
+      loader: "jsx",
+      target: "es2015",
+    });
+
+    setCode(result.code);
+  };
+
+  return (
+    <div>
+      <textarea
+        onChange={(event: ChangeEvent<HTMLTextAreaElement>) =>
+          setInput(event.target.value)
+        }
+      />
+      <div>
+        <button onClick={onButtonClick}>Submit</button>
+      </div>
+      <div>
+        <pre>{code}</pre>
+      </div>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
